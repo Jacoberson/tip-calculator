fefb278b41433db25b9850d1d5f77accbffe657f
Jacoberson <c.jacob.moore92@gmail.com>
2021-09-10 12:15:37 -0500

    enhancement: background

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../images/logo.svg";
+
+const Header = () => {
+  return (
+    <div className="logo">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
+
+export default Header;
