ff52d3bed37d68602130debaaf6a70e18f267e36
Jacoberson <c.jacob.moore92@gmail.com>
2021-09-10 18:00:30 -0500

    enhancement: bill portion of form

---
+++ src/components/TipForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TipForm = () => {
+  return (
+    <form className="bill-portion">
+      <p className="bill">Bill</p>
+      <input id="bill-input" type="number" />
+    </form>
+  );
+};
+
+export default TipForm;
